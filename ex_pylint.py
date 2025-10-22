f374fd7012ee4e9b5b83c2f9a74b4834f823b598
Davi Oliveira <davioliveiraes7@gmail.com>
2025-10-22 09:46:41 -0300

    config: Adding pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,8 @@
+print("Olá mundo")
+
+def minha_funcao():
+    print("Estou na minha função")
+
+# snake_case -> Variáveis e funções em python - my_variable
+# cammelCase -> Errado de acordo com as documentações em python - minhaFuncao
+# PascalCase -> Classes em python - MinhaClasse
