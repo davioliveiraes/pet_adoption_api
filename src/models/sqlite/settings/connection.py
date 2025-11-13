56bda15a62a325c821fcfe8a0fcb7114d33b491a
Davi Oliveira <davioliveiraes7@gmail.com>
2025-11-13 10:37:34 -0300

    config: Correcting database connections and implementing server configurations

--- src/models/sqlite/settings/connection.py
+++ src/models/sqlite/settings/connection.py
@@ -7,7 +7,7 @@ class DBConnectionHandler:
         self.__engine = None
         self.session = None
 
-    def connect_to_be(self):
+    def connect_to_db(self):
         self.__engine = create_engine(self.__connection_string)
 
     def get_engine(self):
